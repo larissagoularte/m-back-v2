4605b95640045db73778d965eba0b90ec2f7a128
Larissa Goularte <58703854+larissagoularte@users.noreply.github.com>
2024-08-01 15:56:32 +0100

    cors

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const connectDB = require('./config/db');
 connectDB();
 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
